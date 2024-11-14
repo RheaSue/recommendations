c6831b384c7556535c32e2d2f1ef294c499552e7
RheaSue <rheasue@outlook.com>
2024-11-14 17:15:28 +0800

    Use HashRouter instead of Router

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom"; // use HashRouter instead of BrowserRouter for better SEO on GitHub
 
 // import './App.css';
 import RecommendationPage from "./pages/RecommendationPage";
